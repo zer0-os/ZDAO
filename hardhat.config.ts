de5327384cd05b774c81c9160ce1a8a6e97c269f
Remscar <remscar@gmail.com>
2021-07-09 12:43:16 -0700

    Added diamond2 example implementation upgraded solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.3",
+        version: "0.8.6",
         settings: {
           optimizer: {
             enabled: true,
