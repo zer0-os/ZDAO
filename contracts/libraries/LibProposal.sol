c50bf26d77875abcfeaf8fae398afa5aaf25b737
Remscar <remscar@gmail.com>
2021-07-21 15:09:48 -0700

    Prototype of zDAO facets

---
+++ contracts/libraries/LibProposal.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+library LibProposal {
+  bytes32 constant PROPOSAL_STORAGE_POSITION =
+    keccak256("zer0.zdao.proposal.storage.v0");
+
+  struct Proposal {
+    address to;
+    uint256 value;
+    bytes data;
+    uint256 snapshotId;
+    uint256 createdOn;
+    bool executed;
+  }
+
+  struct ProposalStorage {
+    mapping(uint256 => Proposal) proposals;
+    uint256 nonce;
+  }
+
+  function proposalStorage()
+    internal
+    pure
+    returns (ProposalStorage storage ds)
+  {
+    bytes32 position = PROPOSAL_STORAGE_POSITION;
+    assembly {
+      ds.slot := position
+    }
+  }
+
+  function createProposal(
+    address to,
+    uint256 value,
+    bytes calldata data,
+    uint256 snapshotId
+  ) internal returns (uint256) {
+    ProposalStorage storage s = proposalStorage();
+
+    s.proposals[++s.nonce] = Proposal({
+      to: to,
+      value: value,
+      data: data,
+      snapshotId: snapshotId,
+      executed: false,
+      createdOn: block.number
+    });
+
+    return s.nonce;
+  }
+
+  function proposalExists(uint256 proposalId) internal view returns (bool) {
+    ProposalStorage storage ps = proposalStorage();
+    return ps.nonce > proposalId;
+  }
+
+  function proposalDetails(uint256 proposalId)
+    internal
+    view
+    returns (Proposal storage proposal)
+  {
+    ProposalStorage storage ps = proposalStorage();
+    proposal = ps.proposals[proposalId];
+  }
+}
---
+++ contracts/libraries/LibZDAOCore.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {IZeroToken} from "../interfaces/IZeroToken.sol";
+
+library LibZDAOCore {
+  bytes32 constant ZDAO_CORE_STORAGE_POSITION =
+    keccak256("zer0.zdao.core.storage.v0");
+
+  struct ZDAOCoreStorage {
+    IZeroToken membershipToken;
+  }
+
+  function zDAOCoreStorage()
+    internal
+    pure
+    returns (ZDAOCoreStorage storage ds)
+  {
+    bytes32 position = ZDAO_CORE_STORAGE_POSITION;
+    assembly {
+      ds.slot := position
+    }
+  }
+
+  function membershipToken()
+    internal
+    view
+    returns (IZeroToken membershipToken_)
+  {
+    membershipToken_ = zDAOCoreStorage().membershipToken;
+  }
+
+  function requireSelfCall() internal view {
+    require(msg.sender == address(this), "ZDAO: 1000");
+  }
+}
+
+contract ZDAO {
+  modifier onlySelf() {
+    LibZDAOCore.requireSelfCall();
+    _;
+  }
+}
